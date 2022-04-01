781032ef06b47aa80330913eb50eb4930f3b484c
GabrielDongmo <bklyn2451@gmail.com>
2022-04-01 16:12:03 +0100

    tapez sur l'image, et un popup s'affiche vous proposant de sortir une photo de la galerie, ou  d'en prendre une en ouvrant la camera

--- app/src/main/java/com/example/opencamera/MainActivity.kt
+++ app/src/main/java/com/example/opencamera/MainActivity.kt
@@ -12,10 +12,12 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Environment
 import android.provider.MediaStore
+import android.view.Gravity
 import android.view.View
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.Toast
+import androidx.appcompat.widget.PopupMenu
 import androidx.core.content.ContextCompat
 import androidx.core.content.FileProvider
 import java.io.File
@@ -33,33 +35,55 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var cameraButton = findViewById<Button>(R.id.cameraButton)
+        var imageView = findViewById<ImageView>(R.id.imageView)
+        imageView.setOnClickListener {
+            val popupMenu: PopupMenu = PopupMenu(this, imageView)
+            popupMenu.menuInflater.inflate(R.menu.popup_photo_menu, popupMenu.menu)
+
+            popupMenu.setOnMenuItemClickListener(PopupMenu.OnMenuItemClickListener {
+                when(it.itemId) {
+                    R.id.menuItem_openCamera -> openCamera(it.actionView)
+                    R.id.menuItem_openGallery -> selectImage(it.actionView)
+                    else -> openCamera(it.actionView)
+                }
+            })
+
+            popupMenu.show()
+        }
+
+        /*var cameraButton = findViewById<Button>(R.id.cameraButton)
         cameraButton.setOnClickListener {
-            val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-            photoFile = getPhotoFile(FILE_NAME)
-            var imageView = findViewById<ImageView>(R.id.imageView)
-
-            //takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoFile)
-            val fileProvider = FileProvider.getUriForFile(this, "edu.standard.rkpandey.fileprovider", photoFile)
-            takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider)
-
-            if (takePictureIntent.resolveActivity(this.packageManager) != null) {
-                imageView.setImageBitmap(null)
-                startActivityForResult(takePictureIntent, REQUEST_CODE)
-            }
-            else {
-                Toast.makeText(this, "Unable to open camera", Toast.LENGTH_SHORT).show()
-            }
+            openCamera(it)
         }
 
         var galleryButton = findViewById<Button>(R.id.galleryButton)
         galleryButton.setOnClickListener {
             selectImage(it)
-        }
+        }*/
 
     }
 
-    private fun selectImage(view: View?) {
+    private fun openCamera(view: View?): Boolean {
+        val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        photoFile = getPhotoFile(FILE_NAME)
+        var imageView = findViewById<ImageView>(R.id.imageView)
+
+        //takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoFile)
+        val fileProvider = FileProvider.getUriForFile(this, "edu.standard.rkpandey.fileprovider", photoFile)
+        takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider)
+
+        if (takePictureIntent.resolveActivity(this.packageManager) != null) {
+            imageView.setImageBitmap(null)
+            startActivityForResult(takePictureIntent, REQUEST_CODE)
+        }
+        else {
+            Toast.makeText(this, "Unable to open camera", Toast.LENGTH_SHORT).show()
+        }
+
+        return true
+    }
+
+    private fun selectImage(view: View?): Boolean {
         if(ContextCompat.checkSelfPermission(applicationContext,
                 android.Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
             requestPermissions(arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE), 1)
@@ -67,6 +91,8 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
             startActivityForResult(intent, 2)
         }
+
+        return true
     }
 
     override fun onRequestPermissionsResult(
